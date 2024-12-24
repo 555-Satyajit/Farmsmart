7929f0bb408363049a7346900719e3559103913e
555-Satyajit <satyajitpujapanda9@gmail.com>
2024-12-24 11:49:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LandingPage from './LandingPage'; // Importing the LandingPage component
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage /> {/* Rendering the LandingPage component */}
+    </div>
+  );
+}
+
+export default App;
